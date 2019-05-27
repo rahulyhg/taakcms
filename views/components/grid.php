5231e985b9a6dc8dc7fe1b376ae1f1619fc9b4a6
alireza <a.edalatpour@gmail.com>
2019-05-27 20:18:30 +0430

    fix bug in grid

--- views/components/grid.php
+++ views/components/grid.php
@@ -16,8 +16,11 @@
         <tr>
             <td scope="row"><button type="button" class="btn btn-primary btn-sm" onclick="addRow();">+</button></td>
             <?php foreach($columns as $column) {
-                if ($column->type != 'hidden')
+                if ($column->type == 'hidden'){
+                    echo '<td style="display:none" ></td>';
+                }else{
                     echo '<td></td>';
+                }
             } ?>
         </tr>
     </tbody>
@@ -27,47 +30,82 @@
         var <?php echo $tableId?>_data=[];
         var mytable = $('#<?php echo $tableId?>')[0];
         function getRandomId(){
-            return Math.floor(Math.random() * Math.floor(1000));
+            return Math.floor(Math.random() * Math.floor(1000)) * -1;
         }
 
         function addPreRowToResult(){
             const idField = columnList.filter(c=> c.type == 'hidden')[0];
             const node = $('#' + idField.id);
             if (node.length === 0) return;
-            $('#' + idField.id).val(getRandomId());
-            let currentRow={};
-            for (var i=0;i<columnList.length;i++){
-                currentRow[columnList[i].id] = $('#' + columnList[i].id).val();
+            const rowId = $('#' + idField.id).val();
+            const isNew = rowId == "";
+            
+            if (isNew){
+                $('#' + idField.id).val(getRandomId());
+                let currentRow={};
+                for (var i=0;i<columnList.length;i++){
+                    currentRow[columnList[i].id] = $('#' + columnList[i].id).val();
+                }
+                currentRow['row_status'] = 'inserted';
+                <?php echo $tableId?>_data.push(currentRow);
+            }else{
+                const index = <?php echo $tableId?>_data.findIndex(c=> c.field_id == rowId);
+                for (var i=0;i<columnList.length;i++){
+                    <?php echo $tableId?>_data[index][columnList[i].id] = $('#' + columnList[i].id).val();
+                }
+                if (<?php echo $tableId?>_data[index][idField.id] > 0){
+                    <?php echo $tableId?>_data[index]['row_status'] = 'updated';
+                }
             }
-            <?php echo $tableId?>_data.push(currentRow);
 
-            mytable.rows[mytable.rows.length-2].onclick=(function() {return function() {edit(this.parentElement.parentElement);}})();
+            var row = node[0].parentElement.parentElement;
+            row.onclick=(function() {return function() {edit(this);}})();
 
-            for (var j = 0, col; col = mytable.rows[mytable.rows.length-2].cells[j]; j++) {
+            for (var j = 0, col; col = row.cells[j]; j++) {
                 if (j!=0){
                     const cellValue = col.children[0].value;
                     col.removeChild(col.children[0]);
                     col.appendChild(document.createTextNode(cellValue));
                 }
             } 
+            
+        }
+
+        function changeToEditMode(row){
+            row.onclick=(function() {return function() {}})();
+
+            for (var j = 0, col; col = row.cells[j]; j++) {
+                if (j != 0){
+                    const cellValue = col.innerText;
+                    col.innerText="";
+                    var input = document.createElement('input');
+                    input.type = j==1 ? "hidden" : "text";
+                    input.className = "form-control form-control-sm";
+                    input.id = columnList[j-1].id;
+                    input.value = cellValue;
+                    col.appendChild(input);
+                }
+            } 
         }
 
         function edit(row){
-            
-
+            addPreRowToResult();
+            changeToEditMode(row);
         }
 
         function addRow(){
             addPreRowToResult();
             
             var newRow = $("<tr></tr>");
-            for (var i=0;i<7;i++){
+            for (var i=0;i<columnList.length+1;i++){
                 var newelement = $("<td></td>");
                 if (i==0){
                     var btn = document.createElement('button');
                     btn.innerText = "-";
                     btn.className = "btn btn-danger btn-sm";
-                    btn.onclick=(function() {return function() {removeRow(this.parentElement.parentElement);}})();
+                    btn.onclick=(function() {return function() {
+                        removeRow(this.parentElement);
+                        }})();
                     btn.type="button";
                     newelement.append(btn);
                 } else if (i==1){
@@ -91,12 +129,25 @@
         }
 
         function removeRow(row){
+            var e = window.event;
+            e.cancelBubble = true;
+            if (e.stopPropagation) e.stopPropagation();
+            
             for( var i = 0; i < <?php echo $tableId?>_data.length; i++){ 
-                if ( <?php echo $tableId?>_[i].field_id == row.children[1].innerText) {
-                    <?php echo $tableId?>_data.splice(i, 1); 
+                isNew = Number(row.parentElement.children[1].innerText) < 0;
+                if ( <?php echo $tableId?>_data[i].field_id == row.parentElement.children[1].innerText) {
+                    if (isNew)
+                        <?php echo $tableId?>_data.splice(i, 1); 
+                    else
+                        <?php echo $tableId?>_data[i]['row_status'] = 'deleted';
                 }
             }
-            row.remove();
+
+            if (row.parentElement){
+                row.parentElement.remove();
+            }else{
+                row.remove();
+            }
         }
 
         function getTableData(){
